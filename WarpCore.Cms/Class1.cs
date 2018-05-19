14035d489fa1f7a2638273930d2c01a9de1b22a6
Aaron Henselin <aaron.henselin@catalystfire.com>
2018-05-19 15:45:43 -0400

    cms

---
+++ WarpCore.Cms/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WarpCore.Cms
+{
+    public enum PageType { ContentPage,GroupingPage,Redirect}
+
+    [Table("cms_page")]
+    public class Page
+    {
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public Guid? ParentPageId { get; set; }
+
+        [Column]
+        public Guid SiteId { get; set; }
+
+        [Column]
+        public List<PageContent> PageContent { get; set; } = new List<PageContent>();
+    }
+
+    public class PageContent
+    {
+    }
+
+
+}
---
+++ WarpCore.Cms/Routing/Route.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WarpCore.Cms.Routing
+{
+    public enum RoutePriority
+    {
+        Primary = 0,
+        Former = 10,
+    }
+
+    [Table("cms_route")]
+    public class Route
+    {
+        [Column]
+        public string SitemapPath { get; set; }
+
+        [Column]
+        public int Priority { get; set; }
+
+        [Column]
+        public Guid PageId { get; set; }
+
+        [Column]
+        public Guid? ContentId { get; set; }
+
+        [Column]
+        public string ContentTypeCode{ get; set; }
+    }
+
+    [Table("cms_site")]
+    public class Site
+    {
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public string RoutePrefix { get; set; }
+
+        [Column]
+        public string UriAuthority { get; set; }
+
+        [Column]
+        public int Priority { get; set; }
+
+        
+    }
+
+    public class SiteRepository
+    {
+        public IEnumerable<Site> GetAllSites()
+        {
+            return new List<Site>();
+        }
+    }
+
+    public class RouteRepository
+    {
+        public IEnumerable<Route> GetAllRoutes()
+        {
+            return new List<Route>();
+        }
+    }
+}
