fd74afb029636b6c14a0f7e8def6d174b11f8fcb
Aaron Henselin <aaron.henselin@catalystfire.com>
2018-07-04 23:05:10 -0400

    some js

--- Infrastructure/AzureSearchEngine/Class1.cs
+++ Infrastructure/AzureSearchEngine/Class1.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
 
 namespace AzureSearchEngine
 {
-    public class Class1
-    {
-        public void X()
-        {
-            //var searchIndexClient = new SearchIndexClient();
-            //searchIndexClient.Documents.IndexWithHttpMessagesAsync()
-        }
-    }
+    //public class Class1
+    //{
+    //    public void X()
+    //    {
+    //        var searchIndexClient = new SearchIndexClient();
+    //        var doc = new Document();
+    //        doc.Add("",);
+    //        IndexBatch.MergeOrUpload(new Document[]
+    //        {
+
+
+    //        });
+    //        searchIndexClient.Documents.IndexWithHttpMessagesAsync()
+    //    }
+    //}
 }
