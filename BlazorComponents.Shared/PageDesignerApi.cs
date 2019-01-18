383ce0490e55a0d4cb61dc84be0a0414feac6cb0
Aaron Henselin <aaron.henselin@catalystfire.com>
2019-01-17 23:33:39 -0500

    expanding page design

--- BlazorComponents.Shared/PageDesignerApi.cs
+++ BlazorComponents.Shared/PageDesignerApi.cs
@@ -12,12 +12,29 @@ namespace BlazorComponents.Shared
         LayoutSubtitution,
         GlobalSubstitution
     }
-    public class Node
+
+    public class PageStructure
+    {
+        public List<StructureNode> Nodes { get; set; }
+    }
+
+    public class StructureNode
+    {
+        public Guid Id { get; set; }
+        public string PlacementContentPlaceHolderId { get; set; }
+        public Guid? PlacementLayoutBuilderId { get; set; }
+        public int Order { get; set; }
+        public string WidgetTypeCode { get; set; }
+        public Dictionary<string, string> Parameters { get; set; }
+        public List<StructureNode> AllContent { get; set; } = new List<StructureNode>();
+    }
+
+    public class PreviewNode
     {
         public string FriendlyName { get; set; }
         public Guid ContentId { get; set; }
         public NodeType Type { get; set; }
         public string Html { get; set; }
-        public List<Node> ChildNodes { get; set; } = new List<Node>();
+        public List<PreviewNode> ChildNodes { get; set; } = new List<PreviewNode>();
     }
 }
